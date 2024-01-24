64269f254b926c89314e78ab362b67123d326b2d
lukos123 <max.kabyshev@gmail.com>
2024-01-24 11:11:19 +0100

    add more log

--- app/sendnotification.py
+++ app/sendnotification.py
@@ -18,13 +18,18 @@ async def send_notification(sleep_for):
     while True:
         try:
             now = datetime.datetime.now()
-            log_in_line(f"chas : {now.hour}")
+            log_in_line(f"1chas : {now.hour}")
             await asyncio.sleep(sleep_for)
+            log_in_line(f"2chas : {now.hour}")
+            if now.minute == 15:
+                log("now.minute == 15")
             if now.hour == 9:
                 log("now.hour == 9")
                 name = await db.get_current_user_name()
                 await update.run(0, f"Сьогодні черга {name}")
                 await asyncio.sleep(60*61)
+                log("now.hour == 10")
+
             elif now.hour == 22:
                 log("now.hour == 22")
 
@@ -36,10 +41,13 @@ async def send_notification(sleep_for):
                     await bot.send_message(chat_id, "Сьогодні твоя черга", reply_markup=await kb.get_main_user_keyboard())
 
                 await asyncio.sleep(60*61)
+                log("now.hour == 23")
             elif now.hour == 1:
                 log("now.hour == 1")
 
                 await db.set_days()
                 await asyncio.sleep(60*61)
+                log("now.hour == 2")
         except Exception as e:
+            log("ERROR")
             log(e)
