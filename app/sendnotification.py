24bcb76bc2d7d1fba96701b7f640e41bc1658a91
lukos123 <max.kabyshev@gmail.com>
2024-01-25 20:38:07 +0100

    h m

--- app/sendnotification.py
+++ app/sendnotification.py
@@ -27,16 +27,16 @@ async def send_notification(sleep_for):
                 await asyncio.sleep(60)
                 log("After now.minute == 31")
 
-            if now.hour == 9:
-                log("now.hour == 9", after_line=True)
+            if now.hour == 9 and now.minute == 1:
+                log("now.hour == 9 and now.minute == 1", after_line=True)
                 name = await db.get_current_user_name()
                 await update.run(0, f"Сьогодні черга {name}")
-                log("Before now.hour == 10")
-                await asyncio.sleep(60*61)
-                log("After now.hour == 10")
+                log("Before now.hour == 9 and now.minute == 2")
+                await asyncio.sleep(61)
+                log("After now.hour == 9 and now.minute == 2")
 
-            elif now.hour == 22:
-                log("now.hour == 22", after_line=True)
+            elif now.hour == 22 and now.minute == 1:
+                log("now.hour == 22 and now.minute == 1", after_line=True)
 
                 # name = await db.get_current_user_name()
                 chat_id = await db.get_current_user_chat_id()
@@ -45,16 +45,16 @@ async def send_notification(sleep_for):
                     log(f"bot.send_message({chat_id},'Сьогодні твоя черга')")
                     await bot.send_message(chat_id, "Сьогодні твоя черга", reply_markup=await kb.get_main_user_keyboard())
 
-                log("Before now.hour == 23")
-                await asyncio.sleep(60*61)
-                log("After now.hour == 23")
-            elif now.hour == 1:
-                log("now.hour == 1", after_line=True)
+                log("Before now.hour == 22 and now.minute == 2")
+                await asyncio.sleep(61)
+                log("After now.hour == 22 and now.minute == 2")
+            elif now.hour == 1 and now.minute == 1:
+                log("now.hour == 1 and now.minute == 1", after_line=True)
 
                 await db.set_days()
-                log("Before now.hour == 2")
-                await asyncio.sleep(60*61)
-                log("After now.hour == 2")
+                log("Before now.hour == 1 and now.minute == 2")
+                await asyncio.sleep(61)
+                log("After now.hour == 1 and now.minute == 2")
         except Exception as e:
             log("ERROR", after_line=True)
             log(e)
